bb301d0819fd0f1a29641e24a012765d4f52b9e7
MaximeMORA <pro@maximemora.com>
2019-09-09 12:01:26 +0200

    4

---
+++ controllers/admin-contact.php
@@ -0,0 +1,9 @@
+<?php
+$title = "ADMINISTRATEUR - Nos prospects - Synergy";
+$description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus venenatis mauris nec molestie dapibus. Cras lorem libero, lobortis id iaculis eget, posuere et diam. Quisque tincidunt mi vitae lorem dignissim euismod. Praesent porta eget sem nec bibendum. Phasellus consequat erat sit amet erat aliquet ullamcorper.";
+require_once('./components/start-requires.php');
+require_once('./components/navbar.php');
+//require_once('./views/landing-page.php');
+require_once('./components/footer.php');
+require_once('./components/end-requires.php');
+?>
